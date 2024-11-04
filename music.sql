f2578bd32dece415d351ea141af3198455629853
Brandon Juarez <brandon.2100@live.com>
2024-11-03 22:09:51 -0500

    Add the file to the bundle

--- music.sql
+++ music.sql
@@ -1,1 +1,20 @@
+CREATE TABLE artist (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
 
+CREATE TABLE album (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    artist_id INTEGER,
+    FOREIGN KEY (artist_id) REFERENCES artist(id)
+);
+
+CREATE TABLE song (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    album_id INTEGER,
+    track_number INTEGER,
+    duration INTEGER, -- in seconds
+    FOREIGN KEY (album_id) REFERENCES album(id)
+);
